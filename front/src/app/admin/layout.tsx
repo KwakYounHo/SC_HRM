e51fb1c5a0167548a9958058ab80be9a020735bf
곽윤호 <kwakdm95@gmail.com>
2024-05-18 15:15:30 +0630

    빌드 에러 수정 layout.tsx의 프롭 children타입 지정

--- front/src/app/admin/layout.tsx
+++ front/src/app/admin/layout.tsx
@@ -1,11 +1,12 @@
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import { ComponentProps } from "react";
-import Link from "next/link";
 
-type Props = ComponentProps<"div">;
-
-export default function AdminLayout({ children, ...props }: Props) {
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <div className={"w-full"}>
       <Header />
